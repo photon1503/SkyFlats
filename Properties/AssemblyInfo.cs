d10dc87b94b92bdd4292c45b2ce7920c97ffd16d
Gerald Hitz <g.hitz@gmx.at>
2025-09-13 12:47:30 +0200

    options

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("SkyFlats")]
 // [MANDATORY] A short description of your plugin
-[assembly: AssemblyDescription("SQM based skyflat automation")]
+[assembly: AssemblyDescription("SQM based sky flat automation")]
 
 // The following attributes are not required for the plugin per se, but are required by the official manifest meta data
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 // The repository where your pluggin is hosted
-[assembly: AssemblyMetadata("Repository", "tbd")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/photon1503/SkyFlats")]
 
 // The following attributes are optional for the official manifest meta data
 
@@ -39,19 +39,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("Homepage", "tbd")]
 
 //[Optional] Common tags that quickly describe your plugin
-[assembly: AssemblyMetadata("Tags", "")]
+[assembly: AssemblyMetadata("Tags", "skyflats")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "tbd/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/photon1503/SkyFlats/blob/main/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
-[assembly: AssemblyMetadata("FeaturedImageURL", "")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "https://github.com/photon1503/SkyFlats/blob/670468cd9ae4acea2b738d22cd7ad16169b205d3/skyflats.png?raw=true")]
 //[Optional] A url to an example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //[Optional] An additional url to an example example screenshot of your plugin in action
 [assembly: AssemblyMetadata("AltScreenshotURL", "")]
 //[Optional] An in-depth description of your plugin
-[assembly: AssemblyMetadata("LongDescription", @"tbd")]
+[assembly: AssemblyMetadata("LongDescription", @"This plugin extends the core NINA sky flat sequence functionality with enhanced automation capabilities for precision flat-field calibration. The module introduces SQM-based filter sequencing that automatically triggers flat-field acquisition for each filter at predefined sky brightness levels as measured by a Sky Quality Meter, ensuring optimal exposure conditions for each optical filter throughout the changing twilight sky.
+
+Additionally, the implementation incorporates dynamic null point tracking, which periodically re-slews the telescope to the astronomical null point during capture sequences. This maintains optimal positioning for sky gradient minimization throughout the flat acquisition process, following the methodology established in astronomical literature for obtaining uniform sky background illumination. The combination of these features provides a robust automated solution for obtaining consistent, high-quality flat fields through precision calibration without manual intervention.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
